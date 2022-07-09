6a4a2c13d5eba2fee2ab1c47dc7905410b4a13ad
Jacek <jgrzybek5@gmail.com>
2022-07-09 13:24:27 +0200

    Searching string test

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,5 +1,13 @@
-
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class firstTest {
+    @Test
+    public void demoTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
+        driver.navigate().to("https://www.danfoss.com/en/");
+    }
 }
