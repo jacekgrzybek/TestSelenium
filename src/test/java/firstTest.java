160e40db63e3fe97633a734a5db06e8184c3a59d
Jacek <jgrzybek5@gmail.com>
2022-07-11 20:55:07 +0200

    Searching string test - Test1

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,5 +1,20 @@
-
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class firstTest {
+
+    @Test
+    public void Test1() {
+        //open chrome
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //scenario of test
+        driver.navigate().to("https://www.danfoss.com/en/");
+        WebElement searchText = driver.findElement(By.xpath ("//*[contains(text(),'Explore Danfoss Product Store')]"));
+        //quit chrome
+        driver.quit();
+    }
+
 
 }
